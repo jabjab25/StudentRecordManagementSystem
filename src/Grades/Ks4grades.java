678acc0c321f808f9e733a03d4d0d42151d546b3
Jermaine <Jermaine@LAPTOP-J26TM9IR.home>
2016-04-06 13:10:53 +0100

    added grades to ks4 grades class plus two methods to check if it is a grade and to also return a value

--- src/Grades/Ks4grades.java
+++ src/Grades/Ks4grades.java
@@ -5,10 +5,39 @@
  */
 package Grades;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Jermaine
  */
 public class Ks4grades {
+    private HashMap<String,Integer> grades = new HashMap<>();
     
+    public Ks4grades()
+    {
+        grades.put("U", 0);
+        grades.put("G", 1);
+        grades.put("F", 2);
+        grades.put("E", 3);
+        grades.put("D", 4);
+        grades.put("C", 5);
+        grades.put("B", 6);
+        grades.put("A", 7);
+        grades.put("A*",8);   
+    }
+    public Boolean isGrade(String g)
+    {
+        if(grades.containsKey(g))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    public Integer getValue(String g)
+    {
+        Integer d = grades.get(g);
+        return d;
+    }
 }
