dd05d1cbf35e0af61dc1f3ecd364a84dd0bd3bd8
Jermaine <Jermaine@LAPTOP-J26TM9IR.home>
2016-04-06 13:18:46 +0100

    added ks5 grades again with grades and methods like other grade classes

---
+++ src/Grades/Ks5grades.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grades;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Jermaine
+ */
+public class Ks5grades {
+
+    private HashMap<String, Integer> grades = new HashMap<>();
+
+    public Ks5grades() {
+        grades.put("U", 0);
+        grades.put("E", 1);
+        grades.put("D", 2);
+        grades.put("C", 3);
+        grades.put("B", 4);
+        grades.put("A", 5);
+    }
+
+    public Boolean isGrade(String g) {
+        if (grades.containsKey(g)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Integer getValue(String g) {
+        Integer d = grades.get(g);
+        return d;
+    }
+}
