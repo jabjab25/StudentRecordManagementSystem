389e57b68366de343022e2887bb15f3528ee73bd
Jermaine <Jermaine@LAPTOP-J26TM9IR.home>
2016-04-10 14:28:01 +0100

    trying to get the calc for average grade to work have coded a way i think will work but need to test

--- src/AverageGrade/Ks3AverageGrade.java
+++ src/AverageGrade/Ks3AverageGrade.java
@@ -5,10 +5,91 @@
  */
 package AverageGrade;
 
+import Grades.Ks3grades;
+import ORM.Markedworks;
+import ORM.Students;
+import java.util.ArrayList;
+
 /**
  *
  * @author Jermaine
  */
+
+
 public class Ks3AverageGrade {
+    private Ks3grades kg3 = new Ks3grades();
+    private Markedworks mw;
+    private Students s;
     
+    public Ks3AverageGrade()
+    {
+    
+    }
+    
+    public Ks3AverageGrade(Markedworks m, Students s1){
+        this.mw=m;
+        this.s = s1;
+    }
+    
+    public void setStudnent(Students s1)
+    {
+        this.s = s1;
+    }
+    
+    public void setMarkedWork (Markedworks m)
+    {
+        this.mw = m;
+    }
+    
+    public void calcAverageGrade()
+    {
+        ArrayList<String> gw = getGradedWork();
+        //int index = 0;
+        int no = 0;
+        double count = 0;
+        double value;
+        
+        for(int index = 0; index<gw.size();index++)
+        {
+            String check = gw.get(index);
+            if(!check.equals(""))
+            {
+                double d = kg3.getValue(check);
+                count = count + d;
+                no++;
+            }
+        }
+        value = count/no;
+        System.out.println(value);
+       
+    }
+    
+    private ArrayList getGradedWork()
+    {
+        ArrayList<String> gw = new ArrayList<String>();
+        
+        String g1 = mw.getTask1();
+        String g2 = mw.getTask2();
+        String g3 = mw.getTask3();
+        String g4 = mw.getTask4();
+        String g5 = mw.getTask5();
+        String g6 = mw.getTask6();
+        String g7 = mw.getTask7();
+        String g8 = mw.getTask8();
+        String g9 = mw.getTask9();
+        String g10 = mw.getTask10();
+        
+        gw.add(g1);
+        gw.add(g2);
+        gw.add(g3);
+        gw.add(g4);
+        gw.add(g5);
+        gw.add(g6);
+        gw.add(g7);
+        gw.add(g8);
+        gw.add(g9);
+        gw.add(g10);
+        
+        return gw;
+    }
 }
